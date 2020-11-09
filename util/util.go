94d90bdf929dcf9f17bedd2513c9b6588ef654ef
Your Name <you@example.com>
2020-11-09 23:45:18 +0800

    ok

--- util/util.go
+++ util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"encoding/json"
 	"log"
+	"time"
 )
 
 func ConvJsonStr(v interface{}) string {
@@ -13,4 +14,9 @@ func ConvJsonStr(v interface{}) string {
 func Log(v interface{}) {
 	log.Println(v)
 	return
+}
+
+func Parse(ymdhis string) time.Time {
+	timeObj, _ := time.ParseInLocation("2006-01-02 15:04:05", ymdhis, time.Local)
+	return timeObj
 }

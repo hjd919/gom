2efc234d7c5166a81de19d7e1bb7cd07f434c573
Your Name <you@example.com>
2020-11-11 23:04:38 +0800

    ok

--- util.go
+++ util.go
@@ -4,11 +4,23 @@ import (
 	"encoding/json"
 	"log"
 	"time"
+
+	"github.com/gogf/gf/util/gconv"
 )
 
 func ConvJsonStr(v interface{}) string {
 	b, _ := json.Marshal(v)
 	return string(b)
+}
+
+func JsonEncode(v interface{}) string {
+	b, _ := json.Marshal(v)
+	return string(b)
+}
+
+func JsonDecode(str string, v interface{}) error {
+	err := json.Unmarshal(gconv.Bytes(str), v)
+	return err
 }
 
 func Log(v interface{}) {

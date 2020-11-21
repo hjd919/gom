59f6cd625cda5baa3c4cb5162fe5e8fc304fa5ea
Your Name <you@example.com>
2020-11-22 00:04:12 +0800

    list

--- list.go
+++ list.go
@@ -1,5 +1,21 @@
 package gom
 
+import (
+	"reflect"
+	"strings"
+)
+
+/*
+orderArr := []string{"id desc"}
+orderStr := ""
+if req.Sort != "" {
+	switch req.Sort {
+	case "createtime":
+		orderArr = append(orderArr, "ys_team.id desc")
+}
+orderStr = strings.Join(orderArr, ",")
+*/
+
 func Order(sortType, sortField string) string {
 	if sortType != "" && sortField != "" {
 		return sortType + " " + sortField
@@ -7,6 +23,19 @@ func Order(sortType, sortField string) string {
 		return sortType
 	}
 	return "id desc"
+}
+
+func Fields(point interface{}) (str string) {
+	t := reflect.TypeOf(point).Elem()
+	var orms []string
+	for i := 0; i < t.NumField(); i++ {
+		orms = append(orms, t.Field(i).Tag.Get("orm"))
+	}
+	if len(orms) == 0 {
+		panic("GetFieldsByOrm err")
+	}
+	str = strings.Join(orms, ",")
+	return
 }
 
 func Page(num, size int) (pageNum int, pageSize int) {
